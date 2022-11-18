a35ab911e342c39811db3dc5f0b6e1d43a74048a
Mohammad Nadama <mnk122@student.aru.ac.uk>
2022-11-18 12:12:13 +0000

    delete comments

--- YouInvestMe/Models/Manager.cs
+++ YouInvestMe/Models/Manager.cs
@@ -4,8 +4,5 @@ public class Manager
 {
     public int ID { get; set; }  
     public string? Name { get; set; }  
-    // public string Email { get; set; }  
-    // public string Course { get; set; }  
-    // public string Contact { get; set; }  
     
 }
