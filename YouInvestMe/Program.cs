1b64a14353fe05c006ac294bceff3f1eae214cb8
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-11-20 18:46:32 +0000

    Implement the error page - show page for all 404 status codes

--- YouInvestMe/Program.cs
+++ YouInvestMe/Program.cs
@@ -9,9 +9,9 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
 }
+
+app.UseStatusCodePagesWithReExecute("/Home/Error");
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
