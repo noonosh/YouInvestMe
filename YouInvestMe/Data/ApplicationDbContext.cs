ed1e3d84a4508e7eea62d035d4d059ba4b274cf8
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-11-22 12:54:02 +0000

    add initial migrations for idea model

---
+++ YouInvestMe/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using YouInvestMe.Models;
+
+namespace YouInvestMe.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<YouInvestMe.Models.Idea> Idea { get; set; } = default!;
+    }
+}
