b3420d59440e3b2d8bf1eb89161ebfa23ee9bb47
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-11-22 12:51:02 +0000

    create Idea model

---
+++ YouInvestMe/Models/Idea.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace YouInvestMe.Models
+{
+    public class Idea
+    {
+        public int IdeaId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+    }
+    
+}
