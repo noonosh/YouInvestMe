07afe60b4a019076120dcb7dc3d7316ca74d89b6
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-11-22 12:54:53 +0000

    add new creator model

---
+++ YouInvestMe/Models/Creator.cs
@@ -0,0 +1,11 @@
+namespace YouInvestMe.Models;
+
+public class Creator
+{
+    public int ID { get; set; }
+    
+    public string FirstName { get; set; }
+    
+    public string LastName { get; set; }
+    
+}
