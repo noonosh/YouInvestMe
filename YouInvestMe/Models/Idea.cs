6bd4544b3505224fd82ca15def91b79dfcb8e7c8
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-11-24 21:08:37 +0000

    cleanup main branch / ready for PRs and feature implementations

--- YouInvestMe/Models/Idea.cs
+++ YouInvestMe/Models/Idea.cs
@@ -6,9 +6,9 @@ namespace YouInvestMe.Models
     {
         public int IdeaId { get; set; }
 
-        public string Title { get; set; }
+        public string Title { get; set; } = string.Empty;
 
-        public string Description { get; set; }
+        public string Description { get; set; } = string.Empty;
 
     }
     
